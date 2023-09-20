89826b42e03e2f5865580ef4b2754ff1f96f89f6
Svaagi <jozefsvagerkom5@gmail.com>
2023-09-20 03:48:08 +0200

    fix

--- lib/screens/Login.dart
+++ lib/screens/Login.dart
@@ -54,104 +54,107 @@ Widget build(BuildContext context) {
   return Scaffold(
     backgroundColor: Theme.of(context).primaryColor,
     body: SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          SizedBox(height: 30),
-          Container(
-            child: SvgPicture.asset(
-              'assets/logo.svg',
-              width: 200,
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 30),
+            Container(
+              child: SvgPicture.asset(
+                'assets/logo.svg',
+                width: 200,
+              ),
+              padding: EdgeInsets.all(16),
             ),
-            padding: EdgeInsets.all(16),
-          ),
-          Expanded(
-            child: Center(
-              child: Form(
-                key: _formKey,
-                child: Container(
-                  padding: EdgeInsets.all(16),
-                  width: 300,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        'Prihlásenie',
-                        style: Theme.of(context)
-                            .textTheme
-                            .displayLarge!
-                            .copyWith(
-                              color: Theme.of(context).colorScheme.onPrimary,
-                            ),
-                      ),
-                      SizedBox(height: 100),
-                      reTextField(
-                        "Vložte email",
-                        false,
-                        _emailTextController,
-                        _emailBorderColor,
-                      ),
-                      SizedBox(height: 8),
-                      reTextField(
-                        "Vložte heslo",
-                        true,
-                        _passwordTextController,
-                        _passwordBorderColor,
-                      ),
-                      SizedBox(height: 8),
-                      _errorMessage != null
-                          ? Container(
-                              width: 300,
-                              height: 60,
-                              decoration: BoxDecoration(
-                                color: AppColors.getColor('red').lighter,
-                                borderRadius: BorderRadius.circular(5),
+            Expanded(
+              child: Center(
+                child: Form(
+                  key: _formKey,
+                  child: Container(
+                    padding: EdgeInsets.all(16),
+                    width: 300,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Prihlásenie',
+                          style: Theme.of(context)
+                              .textTheme
+                              .displayLarge!
+                              .copyWith(
+                                color: Theme.of(context).colorScheme.onPrimary,
                               ),
-                              child: Center(
-                                child: Text(
-                                  _errorMessage!,
-                                  style: TextStyle(
-                                    color: AppColors.getColor('red').main,
+                        ),
+                        SizedBox(height: 100),
+                        reTextField(
+                          "Vložte email",
+                          false,
+                          _emailTextController,
+                          _emailBorderColor,
+                        ),
+                        SizedBox(height: 8),
+                        reTextField(
+                          "Vložte heslo",
+                          true,
+                          _passwordTextController,
+                          _passwordBorderColor,
+                        ),
+                        SizedBox(height: 8),
+                        _errorMessage != null
+                            ? Container(
+                                width: 300,
+                                height: 60,
+                                decoration: BoxDecoration(
+                                  color: AppColors.getColor('red').lighter,
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    _errorMessage!,
+                                    style: TextStyle(
+                                      color: AppColors.getColor('red').main,
+                                    ),
                                   ),
                                 ),
+                              )
+                            : Container(),
+                        SizedBox(height: 8),
+                        Text(
+                          'Ak prihlasovacie údaje nemáš, vypýtaj ',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium!
+                              .copyWith(
+                                color: Theme.of(context).colorScheme.onPrimary,
                               ),
-                            )
-                          : Container(),
-                      SizedBox(height: 8),
-                      Text(
-                        'Ak prihlasovacie údaje nemáš, vypýtaj ',
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyMedium!
-                            .copyWith(
-                              color: Theme.of(context).colorScheme.onPrimary,
-                            ),
-                      ),
-                      Text(
-                        'si ich od svojho vyučujúceho ',
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyMedium!
-                            .copyWith(
-                              color: Theme.of(context).colorScheme.onPrimary,
-                            ),
-                      ),
-                    ],
+                        ),
+                        Text(
+                          'si ich od svojho vyučujúceho ',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium!
+                              .copyWith(
+                                color: Theme.of(context).colorScheme.onPrimary,
+                              ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          ReButton(
-            activeColor: AppColors.getColor('green').main,
-            defaultColor: AppColors.getColor('green').light,
-            disabledColor: AppColors.getColor('mono').lightGrey,
-            focusedColor: AppColors.getColor('primary').lighter,
-            hoverColor: AppColors.getColor('green').main,
-            text: 'PRIHLÁSIŤ',
-            onTap: handleLogin,
-          ),
-          SizedBox(height: 30),
-        ],
+            ReButton(
+              activeColor: AppColors.getColor('green').main,
+              defaultColor: AppColors.getColor('green').light,
+              disabledColor: AppColors.getColor('mono').lightGrey,
+              focusedColor: AppColors.getColor('primary').lighter,
+              hoverColor: AppColors.getColor('green').main,
+              text: 'PRIHLÁSIŤ',
+              onTap: handleLogin,
+            ),
+            SizedBox(height: 30),
+          ],
+        ),
       ),
     ),
   );
